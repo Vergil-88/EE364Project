155877bf87aa5daa4b81fc085769ed44aa8510de
AhmedRE14 <ahmed.r.14222@gmail.com>
2023-01-18 20:59:04 +0300

    more prints just becouse

--- tset.java
+++ tset.java
@@ -2,5 +2,17 @@ public class tset{
     public static void main(String[] args) {
         System.out.println("bingo");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+
     }
 }
