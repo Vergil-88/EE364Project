bf96eb73d272238e3b4e0b12dcb73995d5d87044
AhmedRE14 <ahmed.r.14222@gmail.com>
2023-01-19 23:11:23 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,64 @@
+public class Main {
+public static void main(String[] args) {
+//           (Genrating Objects )    
+                                /*Genertings Cites and Regins*/
+                                
+                                
+                                
+                                
+                                
+                                
+                                /*Genrtings Pepole */
+                                
+
+
+
+
+
+
+
+
+
+
+//           (Main Loop ) 
+
+/*-MAIN_loop(Evrey_hour)
+0-Normal Life
+  
+
+1- callers declare corona symptomes
+         Random Genration
+ex;10 calls 
+2-Recrded go to Corna Test Center
+
+ex:0.5*10=5 sick
+2.5-type B is confirmed sick and calls people who were around him
+ 
+3-Check up all covid patients
+update the patients status (from the array) every 1 hour
+
+
+ 
+4-Resgeter all sick to normal 
+when done with quarantine        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+    
+}
