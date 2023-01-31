e7e766cfc049502562f820c7975ebc6c844da2b3
AhmedRE14 <ahmed.r.14222@gmail.com>
2023-01-31 21:01:05 +0300

    .

--- ICU.java
+++ ICU.java
@@ -1,5 +1,29 @@
 public class ICU {
-    
+//Delration 
+Human Beds[];
 
-    
+//methods
+public void SetBed(){
+
+
+
+
+
+}
+//////////////////////////////////////////////    
+public int getBed(){
+
+
+
+
+return ;
+}
+////////////////////////////////////////////// 
+public void RemoveBed(){
+
+
+
+
+}
+
 }
--- ministryofHealth.java
+++ ministryofHealth.java
@@ -1,6 +1,65 @@
-
+import java.util.ArrayList;
 
 public class ministryofHealth {
+//Delration 
+
+private ArrayList<Human> Calls = new ArrayList<Human>();
+private ArrayList<Human> PostiveA = new ArrayList<Human>();
+private ArrayList<Human> PostiveB = new ArrayList<Human>();
+private ArrayList<Human> PostiveC = new ArrayList<Human>();
+private ArrayList<Human> Dead = new ArrayList<Human>();
+
+//methods
+
+public void Interacted_A_With_B(){
+
+
+
+
+
+}
+
+//////////////////////////////////////////////
+public void Interacted_B_With_C(){
+
+
+
+
+
+}
+
+//////////////////////////////////////////////
+public void A_to_Dead(){
+
+
+
+
+
+}
+
+//////////////////////////////////////////////
+public void Recovered(){
+
+
+
+
+
+}
+
+//////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
