ac2ef112da140f75a293bfc481d77d1f1a3b9039
Vergil-88 <King-20-vip@hotmail.com>
2023-02-06 02:09:52 +0300

    nothing

--- ICU.java
+++ ICU.java
@@ -1,39 +1,34 @@
 import java.util.ArrayList;
 
 public class ICU {
-//Delration 
-Human Beds[] = new Human [100] ;
+    //Delration 
+    Human Beds[] = new Human [100] ;
 
 //methods
-public void SetBed(Human H , int BedNum){
-for (Human B : Beds){
+    public void SetBed(Human H , int BedNum){
+        for (Human B : Beds){
 
-if(B == null){
-    
-    B = H;
+            if(B == null){
+                
+                B = H;
 
-}
+            }
 
-}
+        }
 
 
 
-}
+    }
 //////////////////////////////////////////////    
-public int getBed(){
+    public int getBed(){
 
-
-
-
-return 1;
-}
+        return 1;
+    }
 ////////////////////////////////////////////// 
-public void RemoveBed(){
+    public void RemoveBed(){
 
 
-
-
-}
+    }
 
 
 }
